e118543532a1f75ad3c443fb414280d53fb4ae4d
NekoMatryoshka <30490996+NekoMatryoshka@users.noreply.github.com>
2018-03-12 20:32:57 +0000

    file util and pojo

--- cloudstorage/src/main/java/uk/ac/herts/jw17aca/cloudstorage/pojo/File.java
+++ cloudstorage/src/main/java/uk/ac/herts/jw17aca/cloudstorage/pojo/File.java
@@ -1,5 +1,70 @@
 package uk.ac.herts.jw17aca.cloudstorage.pojo;
 
+import org.apache.commons.lang.StringUtils;
+
 public class File {
 
+	private int id;
+	private String name;
+	private String directory;
+	private int ownerId;
+	private float size;
+//	private DateTime dateTime;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDirectory() {
+		return directory;
+	}
+
+	public void setDirectory(String directory) {
+		this.directory = directory;
+	}
+
+	public int getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public float getSize() {
+		return size;
+	}
+
+	public void setSize(float size) {
+		this.size = size;
+	}
+
+	public String getPath() {
+		return directory + name;
+	}
+
+	public void setPath(String path) {
+		this.directory = StringUtils.substringBeforeLast(path, "/") + "/";
+		this.directory = StringUtils.substringAfterLast(path, "/");
+	}
+
+	public String getExtension() {
+		return StringUtils.substringAfterLast(name, ".");
+	}
+
+	public void setExtension(String extension) {
+		this.name = StringUtils.substringBeforeLast(name, ".") + extension;
+	}
 }
--- cloudstorage/src/main/java/uk/ac/herts/jw17aca/cloudstorage/util/FileUtil.java
+++ cloudstorage/src/main/java/uk/ac/herts/jw17aca/cloudstorage/util/FileUtil.java
@@ -1,5 +1,22 @@
 package uk.ac.herts.jw17aca.cloudstorage.util;
 
+import java.io.*;
+
 public class FileUtil {
+
+	public static void copyFile(String filePath, String targetPath) throws FileNotFoundException, IOException {
+
+		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(new File(filePath)));
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(targetPath)));
+
+		int len = 0;
+		while ((len = bis.read()) != -1) {
+			bos.write(len);
+		}
+
+		bis.close();
+		bos.close();
+
+	}
 
 }
